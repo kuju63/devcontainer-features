0454a83a925abce1f63a7f905bb7e8ae22f29740
Jun Kurihara <lh182051+src@gmail.com>
2026-06-04 21:50:16 +0900

    feat: Add support for RedHat UBI in CI and install script (#10)

--- src/betterleaks/install.sh
+++ src/betterleaks/install.sh
@@ -3,10 +3,48 @@ set -e
 
 # This file is install script for betterleaks devcontainer feature. It will be download betterleaks binary file from GitHub Releases and install on /usr/local/bin. Need to require to set execution privilege to betterleaks binary file.
 
-# Check pre-requirement: curl or wget
-apt-get update
-if ! [ -x "$(command -v curl)" ] || ! [ -x "$(command -v jq)" ]; then
-    apt-get install -y curl jq
+# Check linux distribution. Supported linux distribution: debian, ubuntu, centos, RedHat Linux(UBI).
+function os_type() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        echo "$ID"
+    else
+        echo "unknown"
+    fi
+}
+
+# Install prerequisite packages. Supported package manager: apt-get, yum, dnf.
+function install_prerequisite() {
+    if [ -x "$(command -v apt-get)" ]; then
+        apt-get update
+        apt-get install -y --no-install-recommends curl ca-certificates tar gzip jq \
+            && apt-get clean \
+            && rm -rf /var/lib/apt/lists/*
+    elif [ -x "$(command -v yum)" ]; then
+        yum install -y curl jq
+    elif [ -x "$(command -v dnf)" ]; then
+        dnf install -y ca-certificates tar gzip jq curl-minimal \
+            && dnf clean all
+    elif [ -x "$(command -v microdnf)" ]; then
+        microdnf install -y ca-certificates tar gzip jq curl-minimal \
+            && microdnf clean all
+    else
+        echo "Error: Unsupported package manager. Supported package manager: apt-get, yum, dnf, microdnf."
+        exit 1
+    fi
+}
+
+OS=$(os_type)
+if [ "$OS" != "debian" ] && [ "$OS" != "ubuntu" ] && [ "$OS" != "centos" ] && [ "$OS" != "rhel" ]; then
+    echo "Error: Unsupported linux distribution $OS. Supported linux distribution: debian, ubuntu, centos, RedHat Linux(UBI)."
+    exit 1
+fi
+
+if ([ -x "$(which curl)" ] || [ -x "$(which wget)" ] || [ -x "$(which curl-minimal)" ]) && [ -x "$(which jq)" ] && [ -x "$(which tar)" ]; then
+    echo "curl, wget, jq, and tar are already installed."
+else
+    echo "Installing prerequisite packages..."
+    install_prerequisite
 fi
 
 # Download betterleaks binary file from GitHub Releases
@@ -35,14 +73,7 @@ else
     exit 1
 fi
 
-if [ $(which curl) ]; then
-    curl -L -o betterleaks.tar.gz "$URL"
-elif [ $(which wget) ]; then
-    wget -O betterleaks.tar.gz "$URL"
-else
-    echo "Error: curl or wget is required to download betterleaks."
-    exit 1
-fi
+curl -L -o betterleaks.tar.gz "$URL"
 
 # Extract betterleaks binary file from tar.gz
 echo "Extracting betterleaks binary file from tar.gz..."
